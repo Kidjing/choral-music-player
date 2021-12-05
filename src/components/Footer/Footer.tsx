af5e2aba08143f1640ddb68bc49e92e844d31488
sjszhdlyl <94841571+sjszhdlyl@users.noreply.github.com>
2021-12-05 17:47:11 +0800

    Add files via upload

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Layout, Button } from "@arco-design/web-react";
+import { IconMenu, IconUp } from '@arco-design/web-react/icon';
+import PlayMode from "./PlayMode/PlayMode";
+import PlayVolume from "./PlayVolume/PlayVolume";
+import PlayControl from "./PlayControl/PlayControl";
+import Song from "./Song/Song";
+
+const Foot = Layout.Footer
+
+const Footer = () => {
+    return(
+        <Foot style={{position:'fixed',width:'100%',bottom:0,right:0,backgroundColor:'#f5f5f4',height:50}}>
+            <Song picture='src\assets\zjl.jpg' songName='songName' singer='singer' isCollected='true' ></Song>
+            <PlayControl></PlayControl>
+            <PlayList></PlayList>
+            <PlayMode></PlayMode>
+            <PlayVolume></PlayVolume>
+            <WordsOfSong></WordsOfSong>
+        </Foot>
+    )
+}
+
+const PlayList = () =>{
+    return(
+        <Link to='/PlayList'>
+            <Button style={{float:'left',height:50,display:'flex',alignItems:'center',border:'none',backgroundColor:'#f5f5f4'}} title='播放列表'>
+                    <IconMenu style={{fontSize:26}} />
+            </Button>
+        </Link>
+        )
+}
+
+const WordsOfSong = () =>{
+    return(
+        <Link to='/WordsOfSong'>
+            <Button  style={{float:'left',height:50,display:'flex',alignItems:'center',border:'none',backgroundColor:'#f5f5f4'}} title='歌词'>
+                <IconUp style={{fontSize:26}} />
+            </Button >
+        </Link>
+    )
+}
+
+export default Footer
