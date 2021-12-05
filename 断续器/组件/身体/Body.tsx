0b7d6f2f07b3f26b19c92dc2a94ac99a6e2905d2
sjszhdlyl <94841571+sjszhdlyl@users.noreply.github.com>
2021-12-05 22:28:12 +0800

    更改了规范

--- 断续器/组件/身体/Body.tsx
+++ 断续器/组件/身体/Body.tsx
@@ -8,19 +8,19 @@ import Album from "./Album/Album";
 import PlayList from "./PlayList/PlayList";
 import WordsOfSong from "./WordsOfSong/WordsOfSong";
 
-const Body = () => {
-    return (
-        <div style={{ position: 'fixed', top: 70, left: 0 }}>
+const Body = () =>{
+    return(
+        <div style={{position:'fixed',top:70,left:0}}>
             <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='/HomePage' element={<HomePage />} />
                 <Route path='/Discover' element={<Discover />} />
-                <Route path='/MusicLibrary' element={<MusicLibrary />} />
+                <Route path='/MusicLibrary' element={<MusicLibrary/>}/>
                 <Route path='/Album' element={<Album />} />
-                <Route path='/SongSheet' element={<SongSheet />} />
-                <Route path='/Singer' element={<Singer />} />
-                <Route path='/PlayList' element={<PlayList />} />
-                <Route path='/WordsOfSong' element={<WordsOfSong />} />
+                <Route path='/SongSheet' element={<SongSheet />}/>
+                <Route path='/Singer' element={<Singer/>} />
+                <Route path='/PlayList'  element={<PlayList/>}/>
+                <Route path='/WordsOfSong' element={<WordsOfSong/>}/>
             </Routes>
         </div>
     )
