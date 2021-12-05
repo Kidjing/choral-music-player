d270491c428e225754585ea0f2d0a3fbccb7d2ec
sjszhdlyl <94841571+sjszhdlyl@users.noreply.github.com>
2021-12-05 22:33:26 +0800

    Update PlayMode.tsx

--- src/components/Footer/PlayMode/PlayMode.tsx
+++ src/components/Footer/PlayMode/PlayMode.tsx
@@ -5,22 +5,20 @@ import { playingStore } from "src/store/reducers/playingReducer";
 
 const PlayMode = () => {
     const [mode,setMode] = React.useState(0);
-    var modes=['PLAY_IN_ORDER','PLAY_IN_SINGLE','PLAY_IN_RANDOM']
-    var playMode =[<div style={{width:30}} title='列表循环'><IconLoop style={{fontSize:26}}/></div>,
-                    <div style={{width:30}} title='单曲循环'><IconLoop style={{fontSize:26}}></IconLoop><text style={{fontSize:10}}>1</text></div>,
-                    <div style={{width:30}} title='随机播放'><IconSwap style={{fontSize:26}}/></div>]
-
+    let modes=['PLAY_IN_ORDER','PLAY_IN_SINGLE','PLAY_IN_RANDOM']
     return(
         <Button style={{float:'left',height:50,display:'flex',alignItems:'center',border:'none',backgroundColor:'#f5f5f4'}}
-        onClick={()=>{
-            setMode((mode+1)%3)
-            const action = {
-                type:'SET_PLAY_MODE',
-                playMode:modes[mode]
-            }
-            playingStore.dispatch(action)
+            onClick={()=>{
+                setMode((mode+1)%3)
+                const action = {
+                    type:'SET_PLAY_MODE',
+                    playMode:modes[mode]
+                }
+                playingStore.dispatch(action)
             }}>
-            {playMode[mode]}
+            if(mode === 0)<div style={{width:30}} title='列表循环'><IconLoop style={{fontSize:26}}/></div>
+            else if(mode === 1)<div style={{width:30}} title='单曲循环'><IconLoop style={{fontSize:26}} /><text style={{fontSize:10}}>1</text></div>
+            else <div style={{width:30}} title='随机播放'><IconSwap style={{fontSize:26}}/></div>
         </Button>
     )
 }
