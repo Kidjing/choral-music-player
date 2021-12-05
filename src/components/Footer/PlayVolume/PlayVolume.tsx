454975f3841c7044b2041a45f06110e7920f2acc
sjszhdlyl <94841571+sjszhdlyl@users.noreply.github.com>
2021-12-05 17:54:55 +0800

    Create PlayVolume.tsx

---
+++ src/components/Footer/PlayVolume/PlayVolume.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Slider } from "@arco-design/web-react";
+import { IconSound, IconMute } from '@arco-design/web-react/icon';
+import { playingStore } from "src/store/reducers/playingReducer";
+
+const PlayVolume = () => {
+    const [volume,setVolume] = React.useState<number | number[]>(30);
+
+    return(
+        <div style={{float:'left',width:'10%',height:50,display:'flex',alignItems:'center'}}>
+            {volume == 0? (
+                <IconMute style={{margin:10,fontSize:40,height:50}}/>
+            ) : (
+                <IconSound style={{margin:10,fontSize:40,height:50}}/>
+            )}
+            <Slider value={volume} onChange={setVolume} onAfterChange={()=>{
+                console.log(volume)
+                const action = {
+                    type:'CHANGE_VOLUME',
+                    playVolume:volume
+                }
+                playingStore.dispatch(action)}}></Slider>
+        </div>
+    )
+}
+
+export default PlayVolume
