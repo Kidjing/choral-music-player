a7b478c45629c03709d19826bf607e2bff27ee62
sjszhdlyl <94841571+sjszhdlyl@users.noreply.github.com>
2021-12-23 15:01:24 +0800

    fix

--- src/views/playlist/index.tsx
+++ src/views/playlist/index.tsx
@@ -1,14 +1,14 @@
 import { MusicTable,CommonCard, TextModal } from 'src/components';
 import React, { useEffect } from 'react';
 import { Button, Space } from '@arco-design/web-react';
-import { IconHeart, IconCaretRight} from '@arco-design/web-react/icon';
+import { IconHeart, IconCaretRight, IconPause} from '@arco-design/web-react/icon';
 import './index.less';
 import { getPlaylistDetail } from 'src/api/songlist';
 import { ISonglistDetail,ITrackId } from 'src/api/types/songlist'
 import { dateTrans } from 'src/utils/timetrans'
 import { IMusic } from 'src/api/types/song'
 import { getSongDetail } from 'src/api/song'
-import { useSearchParams} from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 const Table = (props:{ids:ITrackId[]|undefined}) =>{
     const trackIds = props.ids
@@ -34,9 +34,11 @@ const Table = (props:{ids:ITrackId[]|undefined}) =>{
 
 const Playlist=()=>{
     const [searchParams] = useSearchParams()
+    const navigate = useNavigate();
     let id:number
     const [list, setList] = React.useState<ISonglistDetail>()
     const [heart, setHeart] = React.useState<boolean>(false)
+    const [play, setPlay] = React.useState<boolean>(false)
     useEffect(()=>{
         id = Number(searchParams.get('id'))
         getPlaylistDetail(id).then(res =>{
@@ -50,7 +52,9 @@ const Playlist=()=>{
     return(
         <div className='list'>
             <div className='list-msg'>
-                <div className='list-img'>
+                <div className='list-img'
+                    onClick={()=>{navigate('/playlist/?id=' + list?.id);}}
+                >
                     <CommonCard
                         imgSrc={list?.coverImgUrl as string}
                         title=""
@@ -68,12 +72,31 @@ const Playlist=()=>{
                         />
                     </p>
                     <Space size='large'>
-                        <Button type='primary' icon={<IconCaretRight />}> 播放</Button>
+                        {play?(
+                                <Button
+                                    onClick={()=>{setPlay(!play)}}
+                                    type='primary' 
+                                    icon={<IconPause />}
+                                > 
+                                    暂停
+                                </Button>
+                            ):(
+                                <Button
+                                    onClick={()=>{setPlay(!play)}} 
+                                    type='primary' 
+                                    icon={<IconCaretRight />}
+                                > 
+                                    播放
+                                </Button>
+                        )}
                     </Space>
                     <Space size='large'>
                         <Button style={{marginLeft:20,backgroundColor:'transparent'}} title='收藏'>
-                            {heart? (<IconHeart onClick={()=>{setHeart(!heart)}} style={{fontSize:26,color:'red'}}/>):
-                                (<IconHeart onClick={()=>{setHeart(!heart)}} style={{fontSize:26}}/>)}
+                            {heart? (
+                                <IconHeart onClick={()=>{setHeart(!heart)}} style={{fontSize:26,color:'red'}}/>
+                            ):(
+                                <IconHeart onClick={()=>{setHeart(!heart)}} style={{fontSize:26}}/>
+                            )}
                         </Button>
                     </Space>
                 </div>
