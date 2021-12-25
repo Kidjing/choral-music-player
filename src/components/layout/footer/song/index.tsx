30a9dd51af1bf55dcef6fadaba9830b5b385bb28
sjszhdlyl <94841571+sjszhdlyl@users.noreply.github.com>
2021-12-25 22:52:27 +0800

    fix:收藏

--- src/components/layout/footer/song/index.tsx
+++ src/components/layout/footer/song/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Button } from '@arco-design/web-react';
+import { Button, Alert } from '@arco-design/web-react';
 import { IconHeart } from '@arco-design/web-react/icon';
 import { Track } from 'src/components/track-list';
 import { IMusic } from 'src/api/types/song';
+import { connect } from 'react-redux';
 
 import './index.less';
 
@@ -11,16 +12,30 @@ interface SongMsg {
     isCollected:boolean;
 }
 
-const Song = (props: SongMsg) => {
+const Song = (props: any) => {
     const {song,isCollected}=props
+    const [like, setLike] = React.useState<boolean>(isCollected)
+    const [alert, setAlert] = React.useState<boolean>(false)
     return (
         <div className="current-song">
+            {alert?(
+                <Alert className='alert' closable type='warning'  content='需要登录才能使用该功能' onClose={()=>{setAlert(false)}} />
+            ):(
+                null
+            )}
             <Track album={song} hoverable={false}/>
             <Button
+                onClick={()=>{
+                    if(props.userInfo.status){
+                        setLike(!like)
+                    }else{
+                        setAlert(true)
+                    }
+                }}
                 className="like-btn"
                 title="收藏"
             >
-                {isCollected === true ? (
+                {like === true ? (
                     <IconHeart style={{ fontSize: 20, color: 'red' }} />
                 ) : (
                     <IconHeart style={{ fontSize: 20 }} />
@@ -30,4 +45,10 @@ const Song = (props: SongMsg) => {
     );
 };
 
-export default Song;
+const mapStateToProps = (state: any) => {
+    return {
+        userInfo: state.userInfoReducer,
+    };
+};
+
+export default connect(mapStateToProps)(Song);
